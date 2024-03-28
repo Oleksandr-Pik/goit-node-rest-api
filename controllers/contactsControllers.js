3158ea5f4ffe69a9b9919fe3cab37cd6cd45309d
Oleksandr-Pik <pik.aleksandr79@gmail.com>
2024-03-28 20:49:29 +0200

    1

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,48 @@
-import contactsService from "../services/contactsServices.js";
+import { HttpError, ctrlWrapper } from "../helpers/index.js";
 
-export const getAllContacts = (req, res) => {};
+import * as contactsService from "../services/contactsServices.js";
 
-export const getOneContact = (req, res) => {};
+const getAllContacts = async (_, res) => {
+  const result = await contactsService.listContacts();
+  res.json(result);
+};
 
-export const deleteContact = (req, res) => {};
+const getOneContact = async (req, res) => {
+    const { id } = req.params;
+    const result = await contactsService.getContactById(id);
+    if (!result) {
+      throw HttpError(404, `Contact with id=${id} not found`);
+    }
+    res.json(result);
+};
 
-export const createContact = (req, res) => {};
+const deleteContact = async (req, res) => {
+    const { id } = req.params;
+    const result = await contactsService.removeContact(id);
+    if (!result) {
+      throw HttpError(404, `Contact with id=${id} not found`);
+    }
+    res.json(result);
+};
 
-export const updateContact = (req, res) => {};
+const createContact = async (req, res) => {
+    const result = await contactsService.addContact(req.body);
+    res.status(201).json(result);
+};
+
+const updateContact = async (req, res) => {
+    const { id } = req.params;
+    const result = await contactsService.updateContactById(id, req.body);
+    if (!result) {
+      throw HttpError(404, `Contact with id=${id} not found`);
+    }
+    res.json(result);
+};
+
+export default {
+  getAllContacts: ctrlWrapper(getAllContacts),
+  getOneContact: ctrlWrapper(getOneContact),
+  deleteContact: ctrlWrapper(deleteContact),
+  createContact: ctrlWrapper(createContact),
+  updateContact: ctrlWrapper(updateContact),
+}
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,28 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-
-})
+  name: Joi.string().required().messages({
+    "any.required": `"name" must be exist`,
+    "string.base": `"name" must be text`,
+  }),
+  email: Joi.string().required().messages({
+    "any.required": `"email" must be exist`,
+    "string.base": `"email" must be text`,
+  }),
+  phone: Joi.string().required().messages({
+    "any.required": `"phone" must be exist`,
+    "string.base": `"phone" must be text`,
+  }),
+});
 
 export const updateContactSchema = Joi.object({
-
-})
+  name: Joi.string().messages({
+    "string.base": `"name" must be text`,
+  }),
+  email: Joi.string().messages({
+    "string.base": `"email" must be text`,
+  }),
+  phone: Joi.string().messages({
+    "string.base": `"phone" must be text`,
+  }),
+});
